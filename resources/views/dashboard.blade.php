5d23a366e058b7e6f8a5bab9079cf55f33238078
Awashousall <awasall@esp.sn>
2024-06-22 19:14:50 +0000

    Mise à jour des fichiers pour inclure X, Y, Z modifications

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,16 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1>Tableau de Bord</h1>
-        <p>Bienvenue sur votre tableau de bord.</p>
+    <div class="container bg-white shadow-lg p-5 rounded mt-5">
+        <h1 class="display-4 text-center text-primary font-italic font-weight-bold">Tableau de Bord</h1>
+        <p class="lead text-center text-primary scrolling-text">Bienvenue sur votre tableau de bord.</p>
         
-        <form action="{{ route('dashboard.action') }}" method="POST">
-            @csrf
-            <div class="form-group">
-                <button type="submit" name="action" value="list_students" class="btn btn-primary">Lister les Étudiants</button>
-                <button type="submit" name="action" value="list_conventions" class="btn btn-secondary">Lister les Conventions</button>
+        <div class="row">
+            <div class="col-12 col-lg-6">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h5 class="card-title text-primary font-italic">Lister les Étudiants</h5>
+                        <p class="card-text">Voir la liste des étudiants.</p>
+                        <form action="{{ route('dashboard.action') }}" method="POST">
+                            @csrf
+                            <button type="submit" name="action" value="list_students" class="btn btn-primary btn-attractive">Lister les Étudiants</button>
+                        </form>
+                    </div>
+                </div>
             </div>
-        </form>
+            
+            <div class="col-12 col-lg-6">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h5 class="card-title text-secondary font-italic">Lister les Conventions</h5>
+                        <p class="card-text">Voir la liste des conventions.</p>
+                        <form action="{{ route('dashboard.action') }}" method="POST">
+                            @csrf
+                            <button type="submit" name="action" value="list_conventions" class="btn btn-secondary btn-attractive">Lister les Conventions</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,31 +4,66 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mon Application</title>
-    <!-- CSS Bootstrap (exemple, vous pouvez ajouter d'autres fichiers CSS ici) -->
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-    <!-- Scripts JavaScript Bootstrap et autres (exemple) -->
-    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+    <!-- CSS Bootstrap -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- Custom CSS -->
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="{{ url('/') }}">Mon Application</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('conventions.index') }}">Conventions</a>
-                    </li>
-                </ul>
-            </div>
+<div class="bg-dark">
+    <div class="container">
+        <div class="row">
+            <nav class="col navbar navbar-expand-lg navbar-dark">
+                <img src="{{ asset('images/Capture1.PNG') }}" width="90" height="90" alt="Logo du site">
+                <a class="navbar-brand text-primary" href="{{ url('/') }}">MediTeam</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarContent">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link text-primary" href="{{ url('/accueil') }}">Accueil</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link text-primary" href="{{ url('/contact') }}">Contact</a>
+                        </li>
+                    </ul>
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link text-primary" href="{{ url('/logout') }}">Logout</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </div>
-    </nav>
+    </div>
+</div>
 
+<div class="background-image">
     <div class="container mt-4">
         @yield('content')
     </div>
+</div>
 
+<div class="bg-dark">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <ul class="list-inline text-center">
+                    <li class="list-inline-item"><a href="#">À propos</a></li>
+                    <li class="list-inline-item">&middot;</li>
+                    <li class="list-inline-item"><a href="#">Vie privée</a></li>
+                    <li class="list-inline-item">&middot;</li>
+                    <li class="list-inline-item"><a href="#">Conditions d'utilisation</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 </body>
 </html>
